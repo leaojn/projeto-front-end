9c84ab125cc22101ad514165f0c016bdbce399a4
João Neto <arealeaoneto@gmail.com>
2021-04-06 16:29:06 -0300

    direcionamento projeto

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,11 +1,12 @@
 import { Heading } from '../../components/Heading';
 import * as Styled from './styles';
+import { FirstPage } from '../../components/FirstPage';
 
 function Home() {
   return (
     <div className="App">
       <Styled.Wrapper background="blue">
-        <Heading>Meu teste de texto</Heading>
+        <FirstPage></FirstPage>
       </Styled.Wrapper>
     </div>
   );
